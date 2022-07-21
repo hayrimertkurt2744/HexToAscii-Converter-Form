ee13e2797600fbfcd9ce20a365cfe2f722ad06dc
Hayri Mert kurt <hayrimertkurt@outlook.com>
2022-07-21 12:03:56 +0300

    Add project files.

---
+++ example/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace example
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rButtonConvertHex = new System.Windows.Forms.RadioButton();
+            this.rBoxConvertAscii = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tBoxDataIn = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rButtonConvertHex
+            // 
+            this.rButtonConvertHex.AutoSize = true;
+            this.rButtonConvertHex.Location = new System.Drawing.Point(28, 19);
+            this.rButtonConvertHex.Name = "rButtonConvertHex";
+            this.rButtonConvertHex.Size = new System.Drawing.Size(44, 17);
+            this.rButtonConvertHex.TabIndex = 0;
+            this.rButtonConvertHex.TabStop = true;
+            this.rButtonConvertHex.Text = "Hex";
+            this.rButtonConvertHex.UseVisualStyleBackColor = true;
+            this.rButtonConvertHex.CheckedChanged += new System.EventHandler(this.rButtonConvertHex_CheckedChanged);
+            // 
+            // rBoxConvertAscii
+            // 
+            this.rBoxConvertAscii.AutoSize = true;
+            this.rBoxConvertAscii.Location = new System.Drawing.Point(28, 65);
+            this.rBoxConvertAscii.Name = "rBoxConvertAscii";
+            this.rBoxConvertAscii.Size = new System.Drawing.Size(52, 17);
+            this.rBoxConvertAscii.TabIndex = 1;
+            this.rBoxConvertAscii.TabStop = true;
+            this.rBoxConvertAscii.Text = "ASCII";
+            this.rBoxConvertAscii.UseVisualStyleBackColor = true;
+            this.rBoxConvertAscii.CheckedChanged += new System.EventHandler(this.rBoxConvertAscii_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rButtonConvertHex);
+            this.groupBox1.Controls.Add(this.rBoxConvertAscii);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // tBoxDataIn
+            // 
+            this.tBoxDataIn.Location = new System.Drawing.Point(12, 118);
+            this.tBoxDataIn.Multiline = true;
+            this.tBoxDataIn.Name = "tBoxDataIn";
+            this.tBoxDataIn.Size = new System.Drawing.Size(200, 130);
+            this.tBoxDataIn.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(225, 260);
+            this.Controls.Add(this.tBoxDataIn);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rButtonConvertHex;
+        private System.Windows.Forms.RadioButton rBoxConvertAscii;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tBoxDataIn;
+    }
+}
+
---
+++ example/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace example
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void rBoxConvertAscii_CheckedChanged(object sender, EventArgs e)
+        {
+            string hex = tBoxDataIn.Text;
+           
+            if (rBoxConvertAscii.Checked) {
+                hex = hex.Replace("-", "");
+                String ascii = "";
+                for (int i = 0; i < hex.Length; i += 2)
+                {
+
+                    // extract two characters from hex string
+                    String part = hex.Substring(i, 2);
+
+                    // change it into base 16 and
+                    // typecast as the character
+                    char ch = (char)Convert.ToInt32(part, 16); ;
+
+                    // add this char to final ASCII string
+                    ascii = ascii + ch;
+                }
+                tBoxDataIn.Text = ascii;
+            }
+            
+            
+
+        }
+        
+        private void rButtonConvertHex_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rButtonConvertHex.Checked)
+            {
+             byte[] ba = Encoding.Default.GetBytes(tBoxDataIn.Text);
+                var hexString = BitConverter.ToString(ba);
+                //hexString = hexString.Replace("-", "");
+                tBoxDataIn.Text = hexString;
+            }
+           
+               
+           
+        }
+    }
+}
